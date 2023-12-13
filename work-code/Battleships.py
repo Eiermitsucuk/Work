9e6b2c62f473875894b1991c13573dd856398728
ImMilan123 <141903508+ImMilan123@users.noreply.github.com>
2023-12-13 13:39:19 +0100

    Update Battleships.py
    added a main method, bug fixes

--- work-code/Battleships.py
+++ work-code/Battleships.py
@@ -1,14 +1,13 @@
 import random
+import time
 
-numbers = list(range(9))
-print(numbers)
-
-print("-"*35)
-print("Welcome to Battleships Game")
-print("-"*35)
-
-board = ["--*--"] * 9
-print(board)
+def start_menu():
+    print("-"*35)
+    print("Welcome to Battleships Game")
+    print("-"*35)
+    print("1. Play Game")
+    print("2. Exit")
+    print("-"*35)
 
 def print_board(board):
     print("-"*35)
@@ -17,44 +16,76 @@ def print_board(board):
     line3 = "+".join(board[6:9])
     print(line1 + "\n" + line2 + "\n" + line3)
 
-print_board(board)
+def play_game():
+    numbers = list(range(9))
+    print(numbers)
 
+    board = ["--*--"] * 9
+    print_board(board)
 
-for i in range(3):
-    ship = random.choice(numbers)
-    numbers.remove(ship)
-    board[ship] = "--A--"
-    print(ship)
+    for i in range(3):
+        ship = random.choice(numbers)
+        numbers.remove(ship)
+        board[ship] = "--A--"
 
-print_board(board)
-screen_board = board.copy()
+    print_board(board)
+    screen_board = board.copy()
 
-for i in range(4):
-    print("-"*35)
-    print("", str(4-i))
-    guess = int(input(
-        """
+    for i in range(5):
+        print("-"*35)
+        print("", str(5-i))
 
-        0-1-2
-        3-4-5
-        6-7-8
+        while True:
+            try:
+                guess = int(input(
+                    """
 
-        Where should we bomb, commander!? =
-        """
-    ))
+                    0-1-2
+                    3-4-5
+                    6-7-8
 
-    if board[guess] == "--A--":
-        print("We hit them !!!")
-        screen_board[guess] = "--H--"
+                    Where should we bomb, commander!? =
+                    """))
+
+                if 0 <= guess <= 8:
+                    break
+                else:
+                    print("Please enter a number between 0 and 8.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+        if board[guess] == "--A--":
+            print("We hit them !!!")
+            screen_board[guess] = "--H--"
+        else:
+            print("That was close but not enough !!!")
+            screen_board[guess] = "--X--"
+
+        print_board(screen_board)
+        print_board(screen_board)
+        time.sleep(3)
+
+    if screen_board.count("--H--") == 3:
+        print("They became food for fish")
     else:
-        print("That was close but not enough !!!")
-        screen_board[guess] = "--X--"
+        print("We lost the war :(")
 
-    print_board(screen_board)
+    print("GAME OVER")
 
-if screen_board.count("--H--") == 3:
-    print("They became food for fish")
-else:
-    print("We lose the war :(")
+def main():
+    while True:
+        start_menu()
+        choice = input("Enter your choice (1 or 2): ")
+
+        if choice == "1":
+            play_game()
+        elif choice == "2":
+            print("Exiting game")
+            break
+        else:
+            print("Invalid choice. Eenter 1 to play and 2 to exit.")
+
+if __name__ == "__main__":
+    main()
 
 print("GAME OVER")
