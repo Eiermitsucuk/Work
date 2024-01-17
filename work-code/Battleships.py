689780565cdfb3f6ad1fd381543f2693bf5052a7
Halil Cankilic <141903706+Eiermitsucuk@users.noreply.github.com>
2024-01-17 10:23:53 +0100

    Update Battleships.py

--- work-code/Battleships.py
+++ work-code/Battleships.py
@@ -1,20 +1,109 @@
 import random
 import time
 
+# Function to display the start menu
 def start_menu():
-    print("-"*45)
-    print("Welcome to Battleships Game")
-    print("-"*45)
+    print("-" * 45)
+    print("""
+
+        
+             !!!     Welcome to Battleships Game     !!!
+
+
+        ⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⡇⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡇⠀⠀⠀⠀⠀⠀⠀⣀⡤⡞⢫⡎⢸⢱⠀⠀⠻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣟⡒⣶⣶⣶⣾⡯⠟⠛⠁⠈⢳⣼⣸⠀⠀⠀⠈⠣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠉⠓⠿⣟⣓⣒⣀⡤⠶⠚⠉⢹⣿⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣟⣿⢿⣶⡗⠒⠒⠒⠒⠲⠶⣶⠶⠶⠦⠼⢿⣦⣀⣀⣀⣀⣀⠀⠀⠈⠳⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⣄⠀⠀⠀⠀⠀⠈⢣⡀⠀⠀⠀⠈⠳⣌⠉⠉⠉⠉⠙⢿⡷⠿⢶⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠝⢆⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠙⢦⡀⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢙⢧⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀⠀⠀⠀⠙⢆⠀⠀⠀⠀⠈⢳⡀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠊⣧⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀⠀⠀⠈⣦⠀⠀⠀⠀⠀⠹⡀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⡆⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠀⠘⣇⠀⠀⠀⠀⠀⢻⡄⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⢸⠀⠀⠀⠀⠀⠀⠸⣦⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠈⢧⠀⠀⠀⠈⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠘⡆⠀⡆⠀⠀⠀⠀⡟⡇⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⢸⣆⠀⠀⠀⠀⠹⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⡇⡀⢰⠀⠀⠀⠀⣷⠃⡇⢠⡄⠀⠀⢸⢸⢠⡄⠀⠘⡟⣿⠀⠀⠀⠀⠀⢸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠟⡿⠀⢸⢀⢀⠀⢠⡟⡔⡇⡌⡇⠀⠀⣺⢸⢸⣿⣰⢀⣧⡿⠀⠀⠀⣀⣴⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣤⣤⠤⢤⣤⣾⠀⡇⡀⡌⣼⣼⠀⣴⣿⡇⡇⣿⡇⡆⣀⣿⣿⣸⢹⣿⢸⣿⠀⢰⠒⠾⣿⣻⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢿⡞⠛⣻⣿⣟⡛⠛⣻⡇⢸⢁⣿⡇⣿⣇⣤⣿⣿⢳⣿⢻⢳⣿⣹⣏⡇⣯⣿⠃⣼⣾⡤⢾⠁⠈⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⣾⣷⣲⣾⣿⣶⣾⢁⡞⣾⣾⣻⣿⣽⣿⠟⣏⣎⡟⣞⣾⣿⣿⢹⣼⣹⣿⣴⠟⠉⠀⣾⠀⢰⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⢯⣿⣿⣿⣻⣿⠃⡼⣹⢳⣿⣿⣽⣿⣿⣾⡘⣹⣽⣻⡿⣿⣣⣷⡿⠛⢻⡾⠷⣄⢀⣿⠀⢸⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡷⠶⠯⠿⣭⡾⢃⡞⣱⢟⣿⣿⣿⣻⣤⣤⣽⣿⡷⣧⣽⡿⢟⣉⡁⠀⠀⢨⢿⣶⣿⣿⠇⢀⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣤⣼⣷⣤⣤⣼⣿⣗⣾⣤⣽⣊⣿⣿⣿⣿⢯⡤⠿⣿⣿⣿⡼⠷⠈⢃⣠⣤⣶⣿⣿⣿⢿⣿⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣍⠉⢹⣿⡿⣿⣿⠿⠿⠛⠛⢹⣯⢹⣿⡟⣆⢸⠿⣠⣿⣿⣻⠧⠖⠛⣿⠖⠋⣽⣿⢽⣶⣿⠣⣶⣿⠃⠀⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣷⣼⣿⣷⣿⣿⣶⣄⣀⣀⣀⣠⣤⣿⣿⠛⠿⠿⣛⣻⡷⠋⠀⠐⠀⣿⣶⢾⣿⠟⣫⢴⠏⢠⣿⢻⣧⣴⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⣤⣀⠀⠀⢀⣀⡉⠉⠉⠛⢛⣛⣿⠿⣿⣶⣾⣿⣩⣅⡤⠶⠶⠛⠉⠙⠛⣿⣛⣭⠏⢠⣿⠻⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢻⣟⡛⠛⠓⠒⠲⠲⠾⠿⠿⠿⠿⠛⠛⠛⠛⠛⠉⠁⠀⣀⣀⣠⣴⣞⡿⢽⣿⠃⣰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠹⣿⡦⣄⣠⣠⣦⣤⣀⣀⣀⣀⣀⣀⣀⣤⣤⡤⠤⠶⠿⠛⣋⣿⣷⣶⡿⡥⣶⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⡿⠋⣼⠿⣻⡿⣿⣻⣟⣦⣄⣀⣠⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣲⣾⣿⣟⣿⣿⠋⣠⣾⠯⢤⣄⡀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⣁⢸⡇⠈⣇⠀⣿⠛⣦⠙⢪⣷⠿⠛⠓⠒⠾⠷⣶⡶⠶⠶⢶⣶⣶⣶⣶⣟⣹⣯⣷⠞⢁⣴⠿⢿⡶⣄⡉⠟⣿⡍⠉⣙⡻⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⢿⢿⠇⢿⣄⠘⠳⠽⠷⢯⡀⢹⣇⠀⠀⣀⡤⠤⠤⢤⣈⡛⢶⣞⠁⢀⣀⠀⠉⠻⠟⠡⠴⠛⠛⠛⠋⠛⢳⣍⠀⠹⣿⡄⠈⠛⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠷⣶⣤⣤⣼⠇⠈⠻⣦⣀⢹⣷⣶⠀⠀⠈⠉⠛⢿⣦⣼⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣏⢀⣾⡿⠣⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀
+
+        """)
+    print("-" * 45)
     print("1. Play Game")
-    print("2. Exit")
-    print("-"*45)
+    print("2. View Highscores")
+    print("3. Exit")
+    print("-" * 45)
 
+# Function to print the game board
 def print_board(board):
-    print("-"*45)
-    for i in range(0, len(board), 4):
-        print("+".join(board[i:i+4]))
+    print("""
 
+                          1    2    3    4    5    6    7    8
+                         ____ ____ ____ ____ ____ ____ ____ ____                
+                        |    |    |    |    |    |    |    |    |                
+                     A  |    |    |    |    |    |    |    |    |                   
+                        |____|____|____|____|____|____|____|____|              
+                        |    |    |    |    |    |    |    |    |
+                     B  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     C  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     D  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     E  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     F  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     G  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+
+                        """)
+    
+    for i in range(0, len(board), 4):
+        print("+".join(board[i:i + 4]))
+
+def save_highscores(score):
+    with open("highscores battleships.txt, "a") as file:
+        file.write(f"{scores}\n")
+
+
+def view_highscores():
+    print("Highscores:")
+    try:
+        with open("highscores battleships.txt", "r") as file:
+            highscores = file.readlines()
+            if not highscores:
+                print("There is no highscores saved yet.")
+            else:
+                for idx, score in enumerate(highscores, start=1):
+                    print(f"{idx}. {int(score)}")
+    except FileNotFoundError:
+        print("There is no highscores saved yet.")
+
+# Function to play the game
 def play_game():
+    score = 0  # Initialize the score
     # Mapping between chess coordinates and index values
     chess_coordinates = {
         'A1': 0, 'A2': 1, 'A3': 2, 'A4': 3,
@@ -23,36 +112,62 @@ def play_game():
         'D1': 12, 'D2': 13, 'D3': 14, 'D4': 15
     }
 
+    # Initialize the game board with '*' representing sea
     board = ["--*--"] * 16
     print_board(board)
 
+    # Place 4 ships randomly on the board
     for i in range(4):
         ship = random.choice(list(chess_coordinates.values()))
-        board[ship] = "--A--"
+        board[ship] = ""
 
+    # Display the board with ships
     print_board(board)
     screen_board = board.copy()
 
+    # Allow the player to make 5 guesses
     for i in range(5):
-        print("-"*45)
-        print("", str(5-i))
+        print("-" * 45)
+        print("", str(5 - i))
 
+        # Get the player's guess and convert it to an index value
         while True:
             try:
                 guess = input(
                     """
-                          A B C D
-                        1 0 0 0 0
-                        2 0 0 0 0
-                        3 0 0 0 0
-                        4 0 0 0 0
+                       
+                          1    2    3    4    5    6    7    8
+                         ____ ____ ____ ____ ____ ____ ____ ____                
+                        |    |    |    |    |    |    |    |    |                
+                     A  |    |    |    |    |    |    |    |    |                   
+                        |____|____|____|____|____|____|____|____|              
+                        |    |    |    |    |    |    |    |    |
+                     B  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     C  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     D  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     E  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     F  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        |    |    |    |    |    |    |    |    |
+                     G  |    |    |    |    |    |    |    |    |
+                        |____|____|____|____|____|____|____|____|
+                        
 
-                    Where should we bomb, commander!? =
-                    """)
+                
 
+                    Where should we bomb, commander!? (Enter coordinates, e.g., A1): """
+                )
                 # Convert chess coordinate to index value
                 guess_index = chess_coordinates.get(guess.upper())
-                
+
                 if guess_index is not None:
                     break
                 else:
@@ -60,23 +175,30 @@ def play_game():
             except ValueError:
                 print("Invalid input. Please enter a valid chess coordinate.")
 
-        if board[guess_index] == "--A--":
+        # Check if the guess hits a ship and update the score
+        if board[guess_index] == "":
             print("We hit them !!!")
             screen_board[guess_index] = "--H--"
+            score += 1
         else:
             print("That was close but not enough !!!")
             screen_board[guess_index] = "--X--"
 
+        # Display the updated board
         print_board(screen_board)
-        time.sleep(3)
+        time.sleep(2)
 
+    # Check if all ships are sunk and display the result
     if screen_board.count("--H--") == 4:
         print("They became food for fish")
     else:
         print("We lost the war :(")
 
+    # Print the final score
+    print(f"Your score: {score}")
     print("GAME OVER")
 
+# Main function to run the game
 def main():
     while True:
         start_menu()
@@ -84,14 +206,18 @@ def main():
 
         if choice == "1":
             play_game()
-        elif choice == "2":
+        elif choice =="2":
+
+        elif choice == "3":
             print("Exiting game")
             break
         else:
             print("Invalid choice. Enter 1 to play and 2 to exit.")
 
+# Entry point of the program
 if __name__ == "__main__":
     main()
 
-print("GAME OVER")
+# Print GAME OVER after exiting the game
+print("GAME OVER"))
 
