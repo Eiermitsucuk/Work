a557cc7f3de19bcd1a3cd0069c5bb6e147bb8d7f
Halil Cankilic <141903706+Eiermitsucuk@users.noreply.github.com>
2024-03-20 13:24:36 +0100

    Update script.js

--- work-code/Battleships/script.js
+++ work-code/Battleships/script.js
@@ -1,22 +1,3 @@
-if (gameMode === 'singlePlayer') {
-      startSinglePlayer()
-    } else {
-      startMultiPlayer()
-    }
-  
-function startSinglePlayer() {
-    generate(shipArray[0])
-    generate(shipArray[1])
-    generate(shipArray[2])
-    generate(shipArray[3])
-    generate(shipArray[4])
-
-    startButton.addEventListener('click', () => {
-        setupButtons.style.display = 'none'
-        playGameSingle()
-    })
-}
-
 function createBoard(grid, squares) {
     for (let i = 0; i < width*width; i++) {
         const square = document.createElement('div')
