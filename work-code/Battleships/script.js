88d9e5a60b765ce2de433f137a8e1940585a2fac
Halil Cankilic <141903706+Eiermitsucuk@users.noreply.github.com>
2024-06-12 12:42:54 +0200

    minor changes

--- work-code/Battleships/script.js
+++ work-code/Battleships/script.js
@@ -1,16 +1,45 @@
+document.querySelectorAll('.ship-preview').forEach(function(ship) {
+    ship.addEventListener('click', function(event) {
+        document.querySelectorAll('.ship-preview').forEach(function(s) {
+            s.classList.remove('selected');
+        });
+        event.target.classList.add('selected');
+        selectedShips = event.target;
+    });
+});
+
 document.addEventListener('keydown', function(event) {
     if (event.key === 'r' || event.key === 'R') {
-        var ships = document.querySelectorAll('.ship-preview');
-            
-        ships.forEach(function(ship) {
-            ship.addEventListener('click', function() {
-                ships.forEach(function(s) {
-                    s.classList.remove('selected');
-                });
-                this.classList.add('selected');
-                this.classList.toggle('rotated');
-            });
-        });
+        if (selectedShips != null) {
+            selectedShips.classList.toggle('rotated');
+        } else {
+            alert('No ships selected')
+        }
     }
- });
+});
+
+const dragShips = document.querySelectorAll('.ship-preview');
+const gameboard = document.getElementById('gameboard');
+
+dragShips.forEach(function(ship) {
+    ship.addEventListener('dragstart', function(event) {
+        event.dataTransfer.setData('text/plain', ship.id);
+    });
+});
+
+gameboard.addEventListener('dragover', function(event) {
+    event.preventDefault();
+})
+
+gameboard.addEventListener('drop', function(event) {
+    event.preventDefault();
+    const shipId = event.dataTransfer.getData('text/plain');
+    const draggedShip = document.getElementById(shipId);
+    if (draggedShip.classList.contains('selected')) {
+        gameboard.appendChild(draggedShip);
+        draggedShip.classList.remove('selected');
+    } else {
+        alert('Please select the ship first to drop it onto gamebaord');
+    }
+});
 
